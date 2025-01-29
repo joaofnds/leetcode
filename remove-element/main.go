3c9b1515bccac12008c998c46a493e72d25ef93b
João Fernandes <joaofnds@joaofnds.com>
2025-01-29 20:07:13 -0300

    add remove-element

---
+++ remove-element/main.go
@@ -0,0 +1,14 @@
+package main
+
+func removeElement(nums []int, val int) int {
+	k := 0
+
+	for _, n := range nums {
+		if n != val {
+			nums[k] = n
+			k++
+		}
+	}
+
+	return k
+}
---
+++ remove-element/main_test.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"reflect"
+	"testing"
+)
+
+func TestRemoveElement(t *testing.T) {
+	tests := []struct {
+		nums     []int
+		val      int
+		expected []int
+	}{
+		{[]int{3, 2, 2, 3}, 3, []int{2, 2}},
+		{[]int{0, 1, 2, 2, 3, 0, 4, 2}, 2, []int{0, 1, 3, 0, 4}},
+	}
+
+	for _, test := range tests {
+		t.Run(fmt.Sprintf("removeElement(%v, %d)", test.nums, test.val), func(t *testing.T) {
+			k := removeElement(test.nums, test.val)
+			if !reflect.DeepEqual(test.nums[:k], test.expected) {
+				t.Errorf("removeElement(%v, %d) got %v, expected %v", test.nums, test.val, test.nums, test.expected)
+			}
+		})
+	}
+}
