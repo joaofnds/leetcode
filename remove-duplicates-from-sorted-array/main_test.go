4a0abf84df896308a1adee2b052287373957ba3e
João Fernandes <joaofnds@joaofnds.com>
2025-01-30 14:09:24 -0300

    add remove-duplicates-from-sorted-array

---
+++ remove-duplicates-from-sorted-array/main_test.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"reflect"
+	"testing"
+)
+
+func TestRemoveDuplicates(t *testing.T) {
+	tests := []struct {
+		nums     []int
+		expected []int
+	}{
+		{[]int{1, 1, 2}, []int{1, 2}},
+		{[]int{0, 0, 1, 1, 1, 2, 2, 3, 3, 4}, []int{0, 1, 2, 3, 4}},
+	}
+
+	for _, test := range tests {
+		t.Run(fmt.Sprintf("removeDuplicates(%v)", test.nums), func(t *testing.T) {
+			k := removeDuplicates(test.nums)
+			if !reflect.DeepEqual(test.nums[:k], test.expected) {
+				t.Errorf("removeDuplicates(%v) got %v, expected %v", test.nums, test.nums, test.expected)
+			}
+		})
+	}
+}
