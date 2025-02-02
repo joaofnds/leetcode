1bc5dcdaef488b3ce8a3d94d3c44959e50714da2
João Fernandes <joaofnds@joaofnds.com>
2025-02-02 19:03:50 -0300

    add roman-to-integer

---
+++ roman-to-integer/main.go
@@ -0,0 +1,32 @@
+package main
+
+var (
+	romanMap = map[byte]int{
+		'I': 1,
+		'V': 5,
+		'X': 10,
+		'L': 50,
+		'C': 100,
+		'D': 500,
+		'M': 1000,
+	}
+)
+
+func romanToInt(s string) int {
+	result := 0
+	prevValue := 0
+
+	for i := len(s) - 1; i >= 0; i-- {
+		currentValue := romanMap[s[i]]
+
+		if currentValue < prevValue {
+			result -= currentValue
+		} else {
+			result += currentValue
+		}
+
+		prevValue = currentValue
+	}
+
+	return result
+}
---
+++ roman-to-integer/main_test.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestRomanToInt(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected int
+	}{
+		{"III", 3},
+		{"LVIII", 58},
+		{"MCMXCIV", 1994},
+	}
+
+	for _, test := range tests {
+		t.Run(fmt.Sprintf("romanToInt(%q)", test.input), func(t *testing.T) {
+			actual := romanToInt(test.input)
+			if actual != test.expected {
+				t.Errorf("romanToInt(%q) got %d, expected %d", test.input, actual, test.expected)
+			}
+		})
+	}
+}
