1cf1a5aaa2e9631e3faa76bc50be9cc46eea0440
João Fernandes <joaofnds@joaofnds.com>
2025-02-04 19:39:28 -0300

    format roman-to-int

--- roman-to-integer/main.go
+++ roman-to-integer/main.go
@@ -1,16 +1,14 @@
 package main
 
-var (
-	romanMap = map[byte]int{
-		'I': 1,
-		'V': 5,
-		'X': 10,
-		'L': 50,
-		'C': 100,
-		'D': 500,
-		'M': 1000,
-	}
-)
+var romanMap = map[byte]int{
+	'I': 1,
+	'V': 5,
+	'X': 10,
+	'L': 50,
+	'C': 100,
+	'D': 500,
+	'M': 1000,
+}
 
 func romanToInt(s string) int {
 	result := 0
