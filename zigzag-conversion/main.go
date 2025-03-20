10092420119f8690bafd3e191345b1966c69aa8f
João Fernandes <joaofnds@joaofnds.com>
2025-03-20 17:35:54 -0300

    [zigzag-conversion] big brain solution

--- zigzag-conversion/main.go
+++ zigzag-conversion/main.go
@@ -1,24 +1,34 @@
 package main
 
-import "strings"
-
 func convert(s string, numRows int) string {
 	if numRows == 1 {
 		return s
 	}
 
-	rows := make([]strings.Builder, numRows)
-	period := 2*numRows - 2
+	k, t, n := 0, 2*numRows-2, len(s)
+	out := make([]byte, n)
 
-	for i := range s {
-		mod := i % period
-		rows[min(mod, period-mod)].WriteByte(s[i])
+	for row := range numRows {
+		for i := 0; ; i++ {
+			index1 := i*t + row
+			if index1 < n {
+				out[k] = s[index1]
+				k++
+			} else {
+				break // no more characters in this row
+			}
+
+			if row != 0 && row != numRows-1 {
+				index2 := (i+1)*t - row
+				if index2 < n {
+					out[k] = s[index2]
+					k++
+				} else {
+					break // no more characters in this row
+				}
+			}
+		}
 	}
 
-	var out strings.Builder
-	out.Grow(len(s))
-	for _, row := range rows {
-		out.WriteString(row.String())
-	}
-	return out.String()
+	return string(out)
 }
